087208a75b7bee4a604878a1288a170ef0c0793a
Colton Allen <allen.colton@hotmail.com>
2021-05-28 12:45:13 -0700

    Day Four 1/2: removed PHP Routing

--- views/Index.view.php
+++ views/Index.view.php
@@ -41,19 +41,25 @@
       <h2 class="section-heading">Get in the Spirit</h2>
     </div>
 
-    <select name="categories" id="category-selector">
-      <option value="Cocktail">Cocktail</option>
-      <option value="Ordinary Drink">Ordinary Drink</option>
-      <option value="Milk / Float / Shake">Milk / Float / Shake</option>
-      <option value="Other/Unknown">Other/Unknown</option>
-      <option value="Cocoa">Cocoa</option>
-      <option value="Shot">Shot</option>
-      <option value="Coffee / Tea">Coffee / Tea</option>
-      <option value="Homemade Liqueur">Homemade Liqueur</option>
-      <option value="Punch / Party Drink">Punch / Party Drink</option>
-      <option value="Beer">Beer</option>
-      <option value="Soft Drink / Soda">Soft Drink / Soda</option>
-    </select>
+    <div class="selector-wrapper">
+      <h2 class="selector-heading">Find the right drink for your mood</h2><br/>
+      <div class="selector">
+        <h3>Select an Occasion:</h3>
+        <select name="categories" id="category-selector">
+          <option value="Cocktail">Cocktail</option>
+          <option value="Ordinary Drink">Ordinary Drink</option>
+          <option value="Milk / Float / Shake">Milk / Float / Shake</option>
+          <option value="Other/Unknown">Other/Unknown</option>
+          <option value="Cocoa">Cocoa</option>
+          <option value="Shot">Shot</option>
+          <option value="Coffee / Tea">Coffee / Tea</option>
+          <option value="Homemade Liqueur">Homemade Liqueur</option>
+          <option value="Punch / Party Drink">Punch / Party Drink</option>
+          <option value="Beer">Beer</option>
+          <option value="Soft Drink / Soda">Soft Drink / Soda</option>
+        </select>
+      </div>
+    </div>
     <div id="drinks-container">
       <!-- DRINK CARDS GO HERE -->
     </div>
